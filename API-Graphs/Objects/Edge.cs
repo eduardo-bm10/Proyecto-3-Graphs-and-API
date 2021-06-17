2b57fb6b6619d0685837d1a7fc7687bf41393612
Eduardo <eduardo.bm10@estudiantec.cr>
2021-06-16 23:19:33 -0600

    Add XML documentation

--- API-Graphs/Objects/Edge.cs
+++ API-Graphs/Objects/Edge.cs
@@ -1,5 +1,10 @@
 namespace API_Graphs.Objects
 {
+    /// <summary>
+    /// La clase <c>Edge</c> genera objetos aristas.
+    /// Contiene un id de arista, un valor para el nodo inicial y final, y su peso.
+    /// </summary>
+    /// Ver <see cref="EdgeController"/> para la clase EdgeController.
     public class Edge
     {
         private int id;
@@ -7,12 +12,27 @@ namespace API_Graphs.Objects
         private int end;
         private int weight;
 
+        /// <summary>
+        /// Constructor de la clase <c>Edge</c> que asigna sus respectivos atributos.
+        /// </summary>
         public Edge(int id, int start, int end, int weight)
         {
             this.id = id;
             this.start = start;
             this.end = end;
             this.weight = weight; 
+        }
+        
+        /// <summary>
+        /// El metodo <c>Id</c> permite acceder al id de la arista.
+        /// </summary>
+        /// <returns>
+        /// El valor del id.
+        /// </returns>
+        public int Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
         }
     }
 }
